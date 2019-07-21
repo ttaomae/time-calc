b8dc979d18104ad3e2242a66da453d759118a652
Todd Taomae <ttaomae@gmail.com>
2019-07-21 10:01:09 -1000

    Print usage and return error codes in main method.

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,27 @@ mod calc;
 mod time;
 
 use std::env;
+use std::process;
 
 use crate::calc::eval::eval;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    if args.len() == 1 {
+        print_usage();
+        process::exit(1);
+    }
+
     match eval(&args[1..].join(" ")) {
         Result::Ok(result) => println!("{}", result),
-        Result::Err(error) => println!("{:?}", error),
+        Result::Err(error) => {
+            eprintln!("{:?}", error);
+            process::exit(2);
+        },
     }
+}
+
+fn print_usage() {
+    eprintln!("Usage: time-calc <expression>");
 }
