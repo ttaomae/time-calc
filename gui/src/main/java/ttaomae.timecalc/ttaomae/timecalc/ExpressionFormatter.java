43025c981b77adf0d407cdd8dc4f9311c5115e2f
Todd Taomae <ttaomae@gmail.com>
2019-10-16 21:03:13 -1000

    Add ExpressionFormatter and tests.

---
+++ gui/src/main/java/ttaomae.timecalc/ttaomae/timecalc/ExpressionFormatter.java
@@ -0,0 +1,201 @@
+package ttaomae.timecalc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class ExpressionFormatter
+{
+    private final ValueFormatter valueFormatter;
+    private final List<Token> tokens;
+    private int nUnclosedParentheses;
+
+    public ExpressionFormatter()
+    {
+        valueFormatter = new ValueFormatter();
+        tokens = new ArrayList<>();
+        nUnclosedParentheses = 0;
+    }
+
+    public void clear()
+    {
+        valueFormatter.clear();
+        tokens.clear();
+        nUnclosedParentheses = 0;
+    }
+
+    public String inputCharacter(char ch)
+    {
+        if (charIsDigit(ch) || ch == '.' || ch == 'n') {
+            inputDigit(ch);
+        }
+        else if (charIsOperator(ch)) {
+            inputOperator(ch);
+        }
+        else if (charIsParenthesis(ch)) {
+            inputParenthesis(ch);
+        }
+
+        return toString();
+    }
+
+    private void inputDigit(char ch)
+    {
+        assert charIsDigit(ch) || ch == '.' || ch == 'n';
+
+        // If empty, pick an arbitrary operator so that empty is handled the same as operators.
+        Token lastToken = getLastToken().orElse(Token.operator('+'));
+
+        // If previous token is empty or operator ...
+        if (lastToken.type == Token.Type.OPERATOR || lastToken.stringValue.equals("(")) {
+            // ... add a new value token.
+            valueFormatter.clear();
+            valueFormatter.inputCharacter(ch);
+            tokens.add(Token.value(valueFormatter.toString()));
+        }
+        // If previous token is time or number ...
+        else if (lastToken.type == Token.Type.VALUE) {
+            // ... update last token.
+            valueFormatter.inputCharacter(ch);
+            setLastToken(Token.value(valueFormatter.toString()));
+        }
+    }
+
+    private void inputOperator(char ch)
+    {
+        assert charIsOperator(ch);
+
+        getLastToken().ifPresent(token -> {
+            // Operators can only follow a value or closing parenthesis.
+            if (token.type == Token.Type.VALUE || token.stringValue.equals(")")) {
+                tokens.add(Token.operator(ch));
+            }
+        });
+    }
+
+    private void inputParenthesis(char ch)
+    {
+        assert charIsParenthesis(ch);
+
+        if (ch == '(') {
+            var lastToken = getLastToken();
+            // Open parentheses can start an expression or follow an operator or open parenthesis.
+            if (lastToken.isEmpty() || lastToken.get().type == Token.Type.OPERATOR
+                    || lastToken.get().stringValue.equals("(")) {
+                tokens.add(Token.parenthesis(ch));
+                nUnclosedParentheses++;
+            }
+        }
+        else if (ch == ')') {
+            // Closing parenthesis only allowed if there are unclosed parentheses.
+            if (nUnclosedParentheses > 0) {
+                var optionalLastToken = getLastToken();
+                // If there are unclosed parentheses, then there must
+                // at least be a previous parenthesis token.
+                assert optionalLastToken.isPresent();
+                var lastToken = optionalLastToken.get();
+
+                // Cannot input closing parenthesis after operator or opening parenthesis.
+                if (lastToken.type != Token.Type.OPERATOR && !lastToken.stringValue.equals("(")) {
+                    tokens.add(Token.parenthesis(ch));
+                    nUnclosedParentheses--;
+                }
+            }
+        }
+    }
+
+    private Optional<Token> getLastToken()
+    {
+        if (tokens.size() == 0) {
+            return Optional.empty();
+        }
+        else {
+            return Optional.of(tokens.get(tokens.size() - 1));
+        }
+    }
+
+    private void setLastToken(Token token)
+    {
+        assert tokens.size() > 0;
+
+        int lastIndex = tokens.size() - 1;
+        tokens.set(lastIndex, token);
+    }
+
+    @Override
+    public String toString()
+    {
+        var result = new StringBuilder();
+
+        var tokenIter = tokens.listIterator();
+        while (tokenIter.hasNext()) {
+            var currentToken = tokenIter.next();
+            result.append(currentToken.stringValue);
+
+            // Append space, if necessary.
+
+            // Never append space if there are no more tokens.
+            if (tokenIter.hasNext()) {
+                var nextToken = tokenIter.next();
+                // Append space after operators, values, and closing parenthesis ...
+                if ((currentToken.type == Token.Type.OPERATOR
+                        || currentToken.type == Token.Type.VALUE
+                        || currentToken.stringValue.equals(")"))
+                        // ... unless the next token is a closing parenthesis.
+                        && !nextToken.stringValue.equals(")")) {
+                    result.append(' ');
+                }
+                // Go back to current token.
+                tokenIter.previous();
+            }
+        }
+        return result.toString();
+    }
+
+    private static boolean charIsDigit(char ch)
+    {
+        return ch >= '0' && ch <= '9';
+    }
+
+    private static boolean charIsOperator(char ch)
+    {
+        return ch == '+' || ch == '-' || ch == '*' || ch == '/';
+    }
+
+    private static boolean charIsParenthesis(char ch)
+    {
+        return ch == '(' || ch == ')';
+    }
+
+    private static class Token
+    {
+        private enum Type
+        {
+            VALUE, OPERATOR, PARENTHESIS
+        }
+
+        private final Type type;
+        private final String stringValue;
+
+        private Token(Type type, String value)
+        {
+            this.type = type;
+            this.stringValue = value;
+        }
+
+        private static Token value(String value)
+        {
+            return new Token(Type.VALUE, value);
+        }
+
+        private static Token operator(char value)
+        {
+            return new Token(Type.OPERATOR, String.valueOf(value));
+        }
+
+        private static Token parenthesis(char value)
+        {
+            return new Token(Type.PARENTHESIS, String.valueOf(value));
+        }
+    }
+}
---
+++ gui/src/test/java/ttaomae.timecalc/ttaomae/timecalc/ExpressionFormatterTest.java
@@ -0,0 +1,169 @@
+package ttaomae.timecalc;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.function.BiConsumer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExpressionFormatterTest
+{
+    @Test
+    public void testInputFirstCharacter()
+    {
+        var formatter = new ExpressionFormatter();
+        assertEquals("", formatter.toString());
+
+        BiConsumer<String, Character> assertInputCharacter = (expected, input) -> {
+            assertEquals(expected, formatter.inputCharacter(input));
+            formatter.clear();
+        };
+
+        assertInputCharacter.accept("0:00:00", '0');
+        assertInputCharacter.accept("0:00:01", '1');
+        assertInputCharacter.accept("0:00:02", '2');
+        assertInputCharacter.accept("0:00:03", '3');
+        assertInputCharacter.accept("0:00:04", '4');
+        assertInputCharacter.accept("0:00:05", '5');
+        assertInputCharacter.accept("0:00:06", '6');
+        assertInputCharacter.accept("0:00:07", '7');
+        assertInputCharacter.accept("0:00:08", '8');
+        assertInputCharacter.accept("0:00:09", '9');
+        assertInputCharacter.accept("0:00:00.0", '.');
+        assertInputCharacter.accept("0n", 'n');
+        assertInputCharacter.accept("(", '(');
+
+        // These characters should be ignored.
+        assertEquals("", formatter.inputCharacter('+'));
+        assertEquals("", formatter.inputCharacter('-'));
+        assertEquals("", formatter.inputCharacter('*'));
+        assertEquals("", formatter.inputCharacter('/'));
+        assertEquals("", formatter.inputCharacter(')'));
+    }
+
+    @Test
+    public void testInputValidExpression()
+    {
+        var formatter = new ExpressionFormatter();
+        assertEquals("", formatter.toString());
+
+        // Input a valid expression.
+        assertInputCharacter("0:00:02", formatter, '2');
+        assertInputCharacter("0:00:24", formatter, '4');
+        assertInputCharacter("0:02:46", formatter, '6');
+        assertInputCharacter("0:02:46 +", formatter, '+');
+        assertInputCharacter("0:02:46 + 0:00:09", formatter, '9');
+        assertInputCharacter("0:02:46 + 0:00:97", formatter, '7');
+        assertInputCharacter("0:02:46 + 0:09:75", formatter, '5');
+        assertInputCharacter("0:02:46 + 0:97:53", formatter, '3');
+        assertInputCharacter("0:02:46 + 9:75:31", formatter, '1');
+        assertInputCharacter("0:02:46 + 97:53:10", formatter, '0');
+        assertInputCharacter("0:02:46 + 97:53:10 *", formatter, '*');
+        assertInputCharacter("0:02:46 + 97:53:10 * (", formatter, '(');
+        assertInputCharacter("0:02:46 + 97:53:10 * ((", formatter, '(');
+        assertInputCharacter("0:02:46 + 97:53:10 * ((0:00:05", formatter, '5');
+        assertInputCharacter("0:02:46 + 97:53:10 * ((5n", formatter, 'n');
+        assertInputCharacter("0:02:46 + 97:53:10 * ((55n", formatter, '5');
+        assertInputCharacter("0:02:46 + 97:53:10 * ((55.0n", formatter, '.');
+        assertInputCharacter("0:02:46 + 97:53:10 * ((55.5n", formatter, '5');
+        assertInputCharacter("0:02:46 + 97:53:10 * ((55.5n -", formatter, '-');
+        assertInputCharacter("0:02:46 + 97:53:10 * ((55.5n - 0n", formatter, 'n');
+        assertInputCharacter("0:02:46 + 97:53:10 * ((55.5n - 1n", formatter, '1');
+        assertInputCharacter("0:02:46 + 97:53:10 * ((55.5n - 18n", formatter, '8');
+        assertInputCharacter("0:02:46 + 97:53:10 * ((55.5n - 18n)", formatter, ')');
+        assertInputCharacter("0:02:46 + 97:53:10 * ((55.5n - 18n) *", formatter, '*');
+        assertInputCharacter("0:02:46 + 97:53:10 * ((55.5n - 18n) * 0:00:03", formatter, '3');
+        assertInputCharacter("0:02:46 + 97:53:10 * ((55.5n - 18n) * 0:00:36", formatter, '6');
+        assertInputCharacter("0:02:46 + 97:53:10 * ((55.5n - 18n) * 0:00:36 /", formatter, '/');
+        assertInputCharacter("0:02:46 + 97:53:10 * ((55.5n - 18n) * 0:00:36 / 0:00:02", formatter, '2');
+        assertInputCharacter("0:02:46 + 97:53:10 * ((55.5n - 18n) * 0:00:36 / 0:00:21", formatter, '1');
+        assertInputCharacter("0:02:46 + 97:53:10 * ((55.5n - 18n) * 0:00:36 / 0:02:15", formatter, '5');
+        assertInputCharacter("0:02:46 + 97:53:10 * ((55.5n - 18n) * 0:00:36 / 0:02:15)", formatter, ')');
+
+        formatter.clear();
+        assertEquals("", formatter.toString());
+
+        // Input an invalid expression.
+        assertInputCharacter("(", formatter, '(');
+        assertInputCharacter("((", formatter, '(');
+        assertInputCharacter("((0n", formatter, 'n');
+        assertInputCharacter("((2n", formatter, '2');
+        assertInputCharacter("((24n", formatter, '4');
+        assertInputCharacter("((248n", formatter, '8');
+        assertInputCharacter("((248.0n", formatter, '.');
+        assertInputCharacter("((248.9n", formatter, '9');
+        assertInputCharacter("((248.93n", formatter, '3');
+        assertInputCharacter("((248.931n", formatter, '1');
+        assertInputCharacter("((248.931n +", formatter, '+');
+        assertInputCharacter("((248.931n + 0:00:05", formatter, '5');
+        assertInputCharacter("((248.931n + 0:00:57", formatter, '7');
+        assertInputCharacter("((248.931n + 0:05:74", formatter, '4');
+        assertInputCharacter("((248.931n + 0:05:74)", formatter, ')');
+        assertInputCharacter("((248.931n + 0:05:74) /", formatter, '/');
+        assertInputCharacter("((248.931n + 0:05:74) / (", formatter, '(');
+        assertInputCharacter("((248.931n + 0:05:74) / (0:00:06", formatter, '6');
+        assertInputCharacter("((248.931n + 0:05:74) / (0:00:06 -", formatter, '-');
+        assertInputCharacter("((248.931n + 0:05:74) / (0:00:06 - 0:00:02", formatter, '2');
+        assertInputCharacter("((248.931n + 0:05:74) / (0:00:06 - 2n", formatter, 'n');
+        assertInputCharacter("((248.931n + 0:05:74) / (0:00:06 - 2.0n", formatter, '.');
+        assertInputCharacter("((248.931n + 0:05:74) / (0:00:06 - 2.2n", formatter, '2');
+        assertInputCharacter("((248.931n + 0:05:74) / (0:00:06 - 2.2n)", formatter, ')');
+        assertInputCharacter("((248.931n + 0:05:74) / (0:00:06 - 2.2n))", formatter, ')');
+        assertInputCharacter("((248.931n + 0:05:74) / (0:00:06 - 2.2n)) *", formatter, '*');
+        assertInputCharacter("((248.931n + 0:05:74) / (0:00:06 - 2.2n)) * (", formatter, '(');
+        assertInputCharacter("((248.931n + 0:05:74) / (0:00:06 - 2.2n)) * (0:00:09", formatter, '9');
+        assertInputCharacter("((248.931n + 0:05:74) / (0:00:06 - 2.2n)) * (0:00:99", formatter, '9');
+        assertInputCharacter("((248.931n + 0:05:74) / (0:00:06 - 2.2n)) * (0:00:99.0", formatter, '.');
+        assertInputCharacter("((248.931n + 0:05:74) / (0:00:06 - 2.2n)) * (0:00:99.9", formatter, '9');
+        assertInputCharacter("((248.931n + 0:05:74) / (0:00:06 - 2.2n)) * (0:00:99.99", formatter, '9');
+
+
+    }
+
+    @Test
+    public void testInputIgnoredCharacters()
+    {
+        var formatter = new ExpressionFormatter();
+
+        assertInputCharacter("(", formatter, '(');
+
+        // Cannot close an empty parenthesss.
+        assertInputCharacter("(", formatter, ')');
+
+        assertInputCharacter("(0:00:03", formatter, '3');
+        assertInputCharacter("(3n", formatter, 'n');
+        assertInputCharacter("(3n *", formatter, '*');
+
+        // Cannot close a parenthesss after operator
+        assertInputCharacter("(3n *", formatter, ')');
+
+        assertInputCharacter("(3n * 0:00:01", formatter, '1');
+        assertInputCharacter("(3n * 0:00:12", formatter, '2');
+        assertInputCharacter("(3n * 0:01:20", formatter, '0');
+        assertInputCharacter("(3n * 0:12:05", formatter, '5');
+        assertInputCharacter("(3n * 0:12:05)", formatter, ')');
+
+        // Cannot input closing parenthesis without matching opening parenthesis.
+        assertInputCharacter("(3n * 0:12:05)", formatter, ')');
+
+        // Cannot input number after closing parenthesis.
+        assertInputCharacter("(3n * 0:12:05)", formatter, '7');
+        assertInputCharacter("(3n * 0:12:05)", formatter, '.');
+        assertInputCharacter("(3n * 0:12:05)", formatter, 'n');
+
+        assertInputCharacter("(3n * 0:12:05) +", formatter, '+');
+        assertInputCharacter("(3n * 0:12:05) + 0:00:04", formatter, '4');
+        assertInputCharacter("(3n * 0:12:05) + 0:00:49", formatter, '9');
+        assertInputCharacter("(3n * 0:12:05) + 0:04:93", formatter, '3');
+        assertInputCharacter("(3n * 0:12:05) + 0:49:36", formatter, '6');
+
+        // Cannot input closing parenthesis without matching opening parenthesis.
+        assertInputCharacter("(3n * 0:12:05) + 0:49:36", formatter, ')');
+    }
+
+    private static void assertInputCharacter(String expected, ExpressionFormatter formatter, char ch)
+    {
+        assertEquals(expected, formatter.inputCharacter(ch));
+        assertEquals(expected, formatter.toString());
+    }
+}
